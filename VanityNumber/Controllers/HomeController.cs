bc70fbd08a267d447ac4de6e29c51ae3dbdd6317
Naveen Chakka <naveen.chakka@infor.com>
2021-06-07 10:16:37 +0530

    inital commit

---
+++ VanityNumber/Controllers/HomeController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VanityNumber.Controllers
+{
+
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+
+        /// <summary>
+        /// +1-541-754-3010
+        /// </summary>
+        /// <remarks>+1-541-754-3010</remarks>
+        /// <param name="phoneNumber"></param>
+        /// <returns></returns>
+        [Route("api/PhoneNumberCombinations")]
+        [HttpGet]
+        public JsonResult PhoneNumberCombinations(string phoneNumber)
+        {
+            JsonResult result = new JsonResult("");
+            Queue<string> queue = new Queue<string>();
+
+
+            if (phoneNumber.Length == 0) { result.Value = phoneNumber; }
+            if (phoneNumber.Length > 0)
+            {
+                string temp;
+                queue.Enqueue("");
+
+                for (int i = 0; i < phoneNumber.Length; i++)
+                {
+                    string x = Helper.MapAlphabet(phoneNumber.Substring(i, 1));
+
+                    while (queue.Peek().Length < i + 1)
+                    {
+                        temp = queue.Dequeue();
+                        foreach (char c in x.ToCharArray())
+                        {
+                            queue.Enqueue(temp + c);
+                        }
+                    }
+                }
+            }
+            result.Value = queue.ToArray();
+            return result;
+        }
+
+
+
+
+        ///// <summary>
+        ///// +1202-555-0271
+        ///// </summary>
+        ///// <remarks></remarks>
+        ///// <param name="phoneNo"></param>
+        ///// <returns></returns>
+        //[Route("api/VanityPhoneNumber")]
+        //[HttpGet]
+        //public JsonResult VanityPhoneNumber(string phoneNo)
+        //{
+        //    JsonResult result = new JsonResult("");
+
+        //    Queue<string> queue = new Queue<string>();
+        //    if (phoneNo.Length > 0)
+        //    {
+        //        for (int i = 0; i < phoneNo.Length; i++)
+        //        {
+
+        //            string temp = "";
+        //            queue.Enqueue("");
+
+        //            string readphone = Helper.MapFirstAlphabet(phoneNo.Substring(i, 1));
+
+        //            while (queue.Peek().Length<i+1)
+        //            {
+        //                temp = queue.Dequeue();
+
+        //                foreach (char c in readphone.ToCharArray())
+        //                {
+        //                    queue.Enqueue(temp + c);
+        //                }
+        //            }
+        //        }
+
+        //        result.Value = queue.ToArray();
+        //    }
+
+
+
+        //    return result;
+        //}
+    }
+}
---
+++ VanityNumber/Helper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VanityNumber.Controllers
+{
+    public static class Helper
+    {
+        public static string MapAlphabet(string v1)
+        {
+            switch (v1)
+            {
+                case "0": return "0";
+                case "1": return "1";
+                case "2": return "abc";
+                case "3": return "def";
+                case "4": return "ghi";
+                case "5": return "jkl";
+                case "6": return "mno";
+                case "7": return "pqrs";
+                case "8": return "tuv";
+                case "9": return "wxyz";
+                default:
+                    return v1;
+            }
+        }
+
+
+
+        public static string MapFirstAlphabet(string v1)
+        {
+            switch (v1)
+            {
+                case "0": return "0";
+                case "1": return "1";
+                case "2": return "a";
+                case "3": return "d";
+                case "4": return "g";
+                case "5": return "j";
+                case "6": return "m";
+                case "7": return "p";
+                case "8": return "t";
+                case "9": return "w";
+                default:
+                    return v1;
+            }
+        }
+
+
+    }
+}
